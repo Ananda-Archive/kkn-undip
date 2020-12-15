12224cbe5921069a0c89d712b85eac27bae3f1c0
Ananda Prabu Tritya Vijaya <anandaprabu.trityavijaya@gmail.com>
2020-12-15 16:34:55 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,7 @@
 import firebase from "firebase";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyByBPM4Gn-9tFt8J-ZGQr4s_kFWFgfzzPs",
-  authDomain: "umkm-srondolkulon.firebaseapp.com",
-  databaseURL: "https://umkm-srondolkulon.firebaseio.com",
-  projectId: "umkm-srondolkulon",
-  storageBucket: "umkm-srondolkulon.appspot.com",
-  messagingSenderId: "836654582400",
-  appId: "1:836654582400:web:8bdccdd400a45a5c373465"
+  
   };
   // Initialize Firebase
   const firebaseApp = firebase.initializeApp(firebaseConfig);
